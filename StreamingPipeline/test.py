9d911f01b3742e37c41cac84092ddada711bd54e
Sid Nimbalkar <sidd.nim@gmail.com>
2020-04-24 10:24:01 -0400

    Delete test.py

--- StreamingPipeline/test.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaConsumer
-import json
-
-consumer = KafkaConsumer('sample1')
-for msg in consumer:
-    tweet_text_json=json.loads(msg.value.decode('utf-8'))
-    print(tweet_text_json)
